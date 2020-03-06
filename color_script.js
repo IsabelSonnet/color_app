8e33601b13c1701c998751a9365d78b570f605c0
Isabel  Sonnet <is73307@srcs.loc>
2020-03-06 13:58:47 -0800

    make boxes moce

--- color_script.js
+++ color_script.js
@@ -1,23 +1,23 @@
 document.addEventListener("dragstart", (event) => {
-  event.dataTransfer.setData("Text", event.target.id);
+    event.dataTransfer.setData("Text", event.target.id);
 });
 
 document.addEventListener("dragover", (event) => {
-  event.preventDefault();
+    event.preventDefault();
 });
 
 document.addEventListener("drop", (event) => {
-  event.preventDefault();
-  if ( event.target.className == "inputone" ) {
-    var data = event.dataTransfer.getData("Text");
-    event.target.appendChild(document.getElementById(data));
-  }
+    event.preventDefault();
+    if (event.target.className == "droptarget") {
+        var data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data));
+    }
 });
 
 document.addEventListener("drop", (event) => {
-  event.preventDefault();
-  if ( event.target.className == "inputtwo" ) {
-    var data = event.dataTransfer.getData("Text");
-    event.target.appendChild(document.getElementById(data));
-  }
+    event.preventDefault();
+    if (event.target.className == "inputtwo") {
+        var data = event.dataTransfer.getData("Text");
+        event.target.appendChild(document.getElementById(data));
+    }
 });
