8e87b4cbe20c65e56f51cd8288291f2464eaf5fa
cauldrone <56094737+cauldrone@users.noreply.github.com>
2020-03-09 14:50:44 -0700

    finished

--- color_script.js
+++ color_script.js
@@ -14,13 +14,23 @@ document.addEventListener("drop", (event) => {
         if (data === "red" && event.target.id  === "inputone") {
             inputone_color = "red";
         }
+        if (data === "yellow" && event.target.id  === "inputone") {
+            inputone_color = "yellow";
+        }
+        if (data === "blue" && event.target.id  === "inputone") {
+            inputone_color = "blue";
+        }
+        if (data === "red" && event.target.id === "inputtwo") {
+            inputtwo_color = "red";
+        }
         if (data === "yellow" && event.target.id === "inputtwo") {
             inputtwo_color = "yellow";
         }
+       if (data === "blue" && event.target.id === "inputtwo") {
+            inputtwo_color = "blue";
+        }
     }
 });
-
-//<img>src = ""</img>
 
 window.addEventListener("keypress", insertColor);
 
@@ -36,26 +46,33 @@ let red = document.getElementById("red");
 let yellow = document.getElementById("yellow");
 let blue = document.getElementById("blue");
 
-//event.target.className == "droptarget"
-
 function insertColor(event) {
     console.log(inputone_color);
     console.log(inputtwo_color);
     if (event.key === "b"){
         if (inputone_color === "red" && inputtwo_color === "yellow"){
+            result.innerHTML = "<div draggable=\"true\" id=\"orange\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"orange\" stroke-width=\"3\" fill=\"orange\" /> </div>";
             document.body.style.background = "orange";
         }
-        if ((inputtwo.innerHTML === red) && (inputone.innerHTML === yellow)){
+        if (inputone_color === "yellow" && inputtwo_color === "red"){
             result.innerHTML = "<div draggable=\"true\" id=\"orange\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"orange\" stroke-width=\"3\" fill=\"orange\" /> </div>";
+          document.body.style.background = "orange";
         }
-        if ((inputone.innerHTML === blue) && (inputtwo.innerHTML === yellow)){
+        if (inputone_color === "blue" && inputtwo_color === "yellow"){
             result.innerHTML = "<div draggable=\"true\" id=\"green\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"green\" stroke-width=\"3\" fill=\"green\" /> </div>";
+          document.body.style.background = "green";
         }
-        if ((inputtwo.innerHTML === blue) && (inputone.innerHTML === yellow)){
+        if (inputone_color === "yellow" && inputtwo_color === "blue"){
             result.innerHTML = "<div draggable=\"true\" id=\"green\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"green\" stroke-width=\"3\" fill=\"green\" /> </div>";
+          document.body.style.background = "green";
         }
-        if ((inputone.innerHTML === blue) && (inputtwo.innerHTML === yellow)){
-            result.innerHTML = "<div draggable=\"true\" id=\"green\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"green\" stroke-width=\"3\" fill=\"green\" /> </div>";
+        if (inputone_color === "red" && inputtwo_color === "blue"){
+            result.innerHTML = "<div draggable=\"true\" id=\"purple\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"purple\" stroke-width=\"3\" fill=\"purple\" /> </div>";
+          document.body.style.background = "purple";
+        }
+       if (inputone_color === "blue" && inputtwo_color === "red"){
+            result.innerHTML = "<div draggable=\"true\" id=\"purple\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"purple\" stroke-width=\"3\" fill=\"purple\" /> </div>";
+         document.body.style.background = "purple";
         }
     }
 }
