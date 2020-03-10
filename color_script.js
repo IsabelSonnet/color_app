dffde95bcd58ea5b62e9894e1d6f067efa1ece80
Em Calderon <calderonhome@Ems-iMac.local>
2020-03-09 20:07:10 -0700

    made title change color and added instructions

--- color_script.js
+++ color_script.js
@@ -53,26 +53,32 @@ function insertColor(event) {
         if (inputone_color === "red" && inputtwo_color === "yellow"){
             result.innerHTML = "<div draggable=\"true\" id=\"orange\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"orange\" stroke-width=\"3\" fill=\"orange\" /> </div>";
             document.body.style.background = "orange";
+            document.getElementById("text").style.color = "orange";
         }
         if (inputone_color === "yellow" && inputtwo_color === "red"){
             result.innerHTML = "<div draggable=\"true\" id=\"orange\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"orange\" stroke-width=\"3\" fill=\"orange\" /> </div>";
-          document.body.style.background = "orange";
+            document.body.style.background = "orange";
+            document.getElementById("text").style.color = "orange";
         }
         if (inputone_color === "blue" && inputtwo_color === "yellow"){
             result.innerHTML = "<div draggable=\"true\" id=\"green\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"green\" stroke-width=\"3\" fill=\"green\" /> </div>";
-          document.body.style.background = "green";
+            document.body.style.background = "green";
+            document.getElementById("text").style.color = "green";
         }
         if (inputone_color === "yellow" && inputtwo_color === "blue"){
             result.innerHTML = "<div draggable=\"true\" id=\"green\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"green\" stroke-width=\"3\" fill=\"green\" /> </div>";
-          document.body.style.background = "green";
+            document.body.style.background = "green";
+            document.getElementById("text").style.color = "green";
         }
         if (inputone_color === "red" && inputtwo_color === "blue"){
             result.innerHTML = "<div draggable=\"true\" id=\"purple\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"purple\" stroke-width=\"3\" fill=\"purple\" /> </div>";
-          document.body.style.background = "purple";
+            document.body.style.background = "purple";
+            document.getElementById("text").style.color = "purple";
         }
        if (inputone_color === "blue" && inputtwo_color === "red"){
             result.innerHTML = "<div draggable=\"true\" id=\"purple\"> <svg width=\"100\" height=\"100\"> <circle class=\"circle\" cx=\"50\" //cy=\"50\" r=\"30\" stroke=\"purple\" stroke-width=\"3\" fill=\"purple\" /> </div>";
-         document.body.style.background = "purple";
+            document.body.style.background = "purple";
+            document.getElementById("text").style.color = "purple";
         }
     }
 }
